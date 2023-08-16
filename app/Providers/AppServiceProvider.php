ba1570f144acde71d379cb349e2a4cee995b04b1
Brandon Enriquez <brandonenr7@gmail.com>
2023-08-16 14:10:23 -0700

    Add new post components

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Blade::anonymousComponentPath(resource_path('views/posts/components'), 'posts');
+
         Model::unguard();
     }
 }
