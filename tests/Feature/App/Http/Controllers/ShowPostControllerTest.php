432917e1770a0244890796edc1be56c42b032fe5
Brandon Enriquez <brandonenr7@users.noreply.github.com>
2023-08-16 23:06:26 -0700

    Update all tests (#4)
    Update all tests
    Add GitHub Action Workflow
    Fix PHPUnit config files
    Implement code coverage

---
+++ tests/Feature/App/Http/Controllers/ShowPostControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Post;
+use function Pest\Laravel\get;
+use App\Http\Controllers\ShowPostController;
+
+it('extends base controller')
+    ->expect(ShowPostController::class)
+    ->toExtend(\App\Http\Controllers\Controller::class);
+
+it('is invokable')
+    ->expect(ShowPostController::class)
+    ->toBeInvokable();
+
+it('shows a post', function () {
+    $post = Post::factory()->create();
+
+    $response = get(route('posts.show', $post))
+        ->assertOk()
+        ->assertViewIs('posts.show')
+        ->assertViewHas('post', $post);
+
+    $response->assertSee($post->title);
+    $response->assertSee($post->body);
+});
+
+it('returns error when model not found', function () {
+    $response = get(route('posts.show', 'invalid-post-title'))
+        ->assertNotFound();
+});
