955c3cbd1d59c84ec1ad4af8a1110fcdd89da263
Brandon Enriquez <brandonenr7@gmail.com>
2023-08-19 01:22:54 -0700

    Add leading back to author footer component #18

--- resources/views/posts/components/author-footer.blade.php
+++ resources/views/posts/components/author-footer.blade.php
@@ -2,7 +2,7 @@
     <img src="https://www.gravatar.com/avatar/{{ md5($user->email) }}?s=256" alt="{{ $user->name }}" class="w-32 h-32 rounded-full" />
     <div class="flex-1 flex flex-col items-center sm:items-start space-y-3">
         <h4 class="text-3xl leading-none tracking-wide">{{ $user->name }}</h4>
-        <p class="text-center sm:text-left text-gray-500 dark:text-gray-400 leading-none">{{ $user->biography }}</p>
+        <p class="text-center sm:text-left text-gray-500 dark:text-gray-400 leading-tight">{{ $user->biography }}</p>
         <ul class="inline-flex items-center space-x-4 text-gray-400 dark:text-gray-600">
             @if ($user->x_url)
                 <li>
