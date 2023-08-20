418bb41fe160ae9b7f749cdc5ed3ac69fb01ef28
Brandon Enriquez <brandonenr7@gmail.com>
2023-08-20 01:53:40 -0700

    Fix whitespace formatting

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,11 +1,11 @@
 <div class="bg-white dark:bg-gray-800 rounded shadow-md">
-        <a href="{{ route('posts.show', $post) }}">
-            <img
-                src="{{ asset('storage/'.$post->image) ?? Vite::asset('resources/img/post-banner-default.png') }}"
-                alt="{{ $post->title }}"
-                class="object-cover rounded-t"
-            />
-        </a>
+    <a href="{{ route('posts.show', $post) }}">
+        <img
+            src="{{ asset('storage/'.$post->image) ?? Vite::asset('resources/img/post-banner-default.png') }}"
+            alt="{{ $post->title }}"
+            class="object-cover rounded-t"
+        />
+    </a>
     <div class="flex flex-col gap-2 p-4">
         <a href="{{ route('posts.show', $post) }}" class="font-semibold text-lg hover:text-indigo-500">
             {{ $post->title }}
