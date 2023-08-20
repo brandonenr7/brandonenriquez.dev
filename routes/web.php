6832d37584b38d9c623f31eab3ab9f55509ea777
Brandon Enriquez <brandonenr7@gmail.com>
2023-08-20 02:11:55 -0700

    Change URL prefix for blog posts

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ShowPostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts/{post:slug}', ShowPostController::class)->name('posts.show');
+Route::get('/blog/{post:slug}', ShowPostController::class)->name('posts.show');
 Route::get('/', HomeController::class)->name('home');
